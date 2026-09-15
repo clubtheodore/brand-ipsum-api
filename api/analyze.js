27f70f5e2a350137dc5d950d1cf9ab2be2c998a0
clubtheodore <theodore.mareschal@gmail.com>
2026-09-15 16:52:20 +0200

    Update cache key format in analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1522,7 +1522,7 @@ const language =
         // On ne récupère donc jamais
         // les anciens résultats V2.
         const cacheKey =
-    `brand-ipsum:v3-34:${locale.toLowerCase()}:${hostname}`
+    `brand-ipsum:v3-35:${locale.toLowerCase()}:${hostname}`
 
         // --------------------------------
         // 1. CACHE REDIS
